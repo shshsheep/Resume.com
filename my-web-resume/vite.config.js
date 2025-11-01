47dba9b4e03c5064813bb59358c20a6c34b4614e
shshsheep <tung.lung38@gmail.com>
2025-11-02 02:46:39 +0800

    Update vite config

--- my-web-resume/vite.config.js
+++ my-web-resume/vite.config.js
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-            tailwindcss()
-          ],
-  base: "https://shshsheep.github.io"
+  plugins: [react(),tailwindcss()],
+  base: "/Resume.com/",
 })
