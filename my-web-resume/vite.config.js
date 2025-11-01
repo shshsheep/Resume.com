8fce113030771501a43c4aa6250c46c99f798389
shshsheep <tung.lung38@gmail.com>
2025-11-02 01:57:01 +0800

    deploy

--- my-web-resume/vite.config.js
+++ my-web-resume/vite.config.js
@@ -6,5 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
             tailwindcss()
-  ],
+          ],
+  base: "https://shshsheep.github.io"
 })
